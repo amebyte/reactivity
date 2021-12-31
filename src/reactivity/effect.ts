c4de8c0b8084ddbf56f230ead358cfce88ab07cb
unknown <416212316@qq.com>
2021-12-31 22:19:14 +0800

    effect && reactive

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,40 @@
+class ReactiveEffect {
+    private _fn: any
+    constructor(fn) {
+        this._fn = fn
+    }
+    run() {
+        activeEffect = this
+        this._fn()
+    }
+}
+let activeEffect
+export function effect(fn) {
+    const _effect  = new ReactiveEffect(fn)
+    _effect.run()
+}
+
+const targetMap = new Map()
+export function track(target, key) {
+    let depsMap = targetMap.get(targetMap)
+    if(!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
+    }
+    let dep = depsMap.get(key)
+    if(!dep) {
+        dep = new Set()
+        depsMap.set(key, dep)
+    }
+    
+    dep.add(activeEffect)
+
+}
+
+export function trigger(target, key) {
+    let depsMap = targetMap.get(target)
+    const deps = depsMap.get(key)
+    deps.forEach(effect => {
+        effect.run()
+    });
+}
