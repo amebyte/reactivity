cfcfcd3e0bfd47917d95f178bc33ba53bd8c5034
unknown <416212316@qq.com>
2022-01-01 17:18:28 +0800

    isReadonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,6 +1,7 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandlers"
 export const enum ReactiveFlags {
-    IS_REACTIVE = "_v_isReactive"
+    IS_REACTIVE = "_v_isReactive",
+    IS_READONLY = "_v_isReadonly"
 }
 export function reactive(raw) {
     return createActiveObject(raw, mutableHandlers)
@@ -12,6 +13,10 @@ export function readonly(raw) {
 
 export function isReactive(value) {
     return !!value[ReactiveFlags.IS_REACTIVE]
+}
+
+export function isReadonly(value) {
+    return !!value[ReactiveFlags.IS_READONLY]
 }
 
 function createActiveObject(raw, baseHandlers) {
