26bda50d3bafdd5622314db1cf2b75b0f76de106
unknown <416212316@qq.com>
2022-01-01 17:10:46 +0800

    isReactive

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,9 +1,13 @@
 import { track, trigger } from "./effect"
+import { ReactiveFlags } from "./reactive"
 const get = createGetter()
 const set = createSetter()
 const readonlyGet = createGetter(true)
 function createGetter(isReadonly = false) {
     return function get (target, key) {
+        if(key === ReactiveFlags.IS_REACTIVE) {
+            return !isReadonly
+        }
         const res = Reflect.get(target, key)
         if(!isReadonly) {
             track(target, key)
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,11 +1,17 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandlers"
-
+export const enum ReactiveFlags {
+    IS_REACTIVE = "_v_isReactive"
+}
 export function reactive(raw) {
     return createActiveObject(raw, mutableHandlers)
 }
 
 export function readonly(raw) {
     return createActiveObject(raw, readonlyHandlers)
+}
+
+export function isReactive(value) {
+    return !!value[ReactiveFlags.IS_REACTIVE]
 }
 
 function createActiveObject(raw, baseHandlers) {
