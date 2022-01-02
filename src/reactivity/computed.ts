128214bb2dbc94fac24643bae97c7195322c7a23
unknown <416212316@qq.com>
2022-01-02 14:57:15 +0800

    实现 computed 计算属性

---
+++ src/reactivity/computed.ts
@@ -0,0 +1,14 @@
+class computedRefImpl {
+    private _getter: any
+    constructor(getter) {
+        this._getter = getter
+    }
+
+    get value() {
+        return this._getter()
+    }
+}
+
+export function computed(getter) {
+    return new computedRefImpl(getter)
+}
